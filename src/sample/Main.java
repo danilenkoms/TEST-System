9595f2730ad9a2485292ef9c57e72862bc7528ed
Danilenko Mariya <65885417+danilenkoms@users.noreply.github.com>
2020-09-23 22:05:31 +0300

    Add files via upload

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Тесты по языка программирования");
+        primaryStage.setTitle("Тесты по языкам программирования");
         primaryStage.setScene(new Scene(root, 747, 460));
         primaryStage.show();
     }
